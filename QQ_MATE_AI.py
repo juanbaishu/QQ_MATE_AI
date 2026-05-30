b752b0a56e31f1efe9fc74e6d5c3d8c5df548d60
juanbai_shu <3589549384@qq.com>
2026-05-30 23:26:50 +0800

    qq图片分析又bug，紧急修复
    腾讯qq有url下载的防盗链机制，原先我直接调用qwen模型在阿里服务器下载qq所在的腾讯服务器上的内容，触发了这个机制，然后有时成功有时失败。
    这次我使用requests浏览器伪装的方式下载 图片内容，然后 使用base64转译，得到图片信息，这次不仅响应速度变快了，还不会丢包了

--- QQ_MATE_AI.py
+++ QQ_MATE_AI.py
@@ -1,18 +1,39 @@
 ﻿import os			                # 将 Python 标准库中的 os 模块引入到当前程序中
 import json			                # json 工具用于 qq消息文本 --convert--> Python格式
 import websocket	                # 连接 Python 和 NapCatqq 的 网络通信协议
+import requests                     # 用于伪装浏览器去下载图片
+import base64                       # 图片转译成文本编码 的 工具
 from openai import OpenAI	        # 从 openai 这个库中直接导入 OpenAI 这个类（或对象）。前者用于调用 openai的 API，后者是 客户端类，用于管理这些 API
+
+#------------------------------函数/方法-----------------------------
+def url_to_base64(image_url):
+    try:
+        # 伪装成浏览器请求，避免被腾讯拦截
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0'}  # HTTP请求头（Request Headers）字典，主要用于模拟浏览器访问，避免被网站识别为爬虫而拦截。
+        response = requests.get(image_url, headers=headers, timeout=10)     # 使用 requests 工具进行伪装，三个参数分别是：图片的 URL 地址、请求头（模拟浏览器）、超时时间 10 秒(超时就报异常)
+        if response.status_code==200:       # http状态码：200 ==> 请求成功
+            # 转化            
+            base64_data = base64.b64encode(response.content).decode('utf-8')      # 使用 base64.   意为   使用 base64库中的方法，b64enable() --> base64格式编码，字符串解码
+            # 返回 经过""中解析方式解析后的  字符串f
+            return f"data:image/jpeg;base64,{base64_data}"      # 这些是 Data URL 协议中的关键字，f是字符串前缀标记，data:是协议标识	表示这是 Data URL，不是 http:// 或 https://
+                                                                # image/jpeg	MIME 类型	告诉浏览器这是 JPEG 图片;base64	编码方式	表示后面的数据是 Base64 编码的
+    except Exception as e:
+        print(f"⚠️ 图片下载失败: {e}")
+    return None
+
+#-------------------------------------------------------------------
+
 
 # ==========================================
 # 1. 配置“大脑”：连接 DeepSeek、阿里通义千问 大模型
 # ==========================================
 # deepseek 用于文字处理
-client = OpenAI(
+client_ds = OpenAI(
     api_key="",      # ⚠️ 记得去后台生成一个全新的安全 Key 填在这里
     base_url="https://api.deepseek.com"                 # 这里的 base_url 代表大模型的云端地址，我一般用https://api.deepseek.com
 )
 # qwen 用于图片识别
-client = OpenAI(
+client_qw = OpenAI(
     api_key="",
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
 )
@@ -45,6 +66,7 @@ def on_message(ws, message):         # won_message：是库定义的关键字参
         # 区分消息类型
         message_text = ""
         message_image_url = None
+        message_image_base64 = ""       # img --> 文本编码
 
         for segment in message_data:
             ai_reply = "这是什么表情包？"           # 防止收到其他类型消息识别不出来而崩溃
@@ -58,7 +80,7 @@ def on_message(ws, message):         # won_message：是库定义的关键字参
                     # 把你的话存入记忆
                     chat_history.append({"role": "user", "content": message_text})         # "user"是API协议中规定的关键字，代表用户说的话
                                     # 呼叫大模型思考回复
-                    ai_response = client.chat.completions.create (         # 调用大模型 API，让它根据 chat_history 里的内容生成回复。
+                    ai_response = client_ds.chat.completions.create (         # 调用大模型 API，让它根据 chat_history 里的内容生成回复。
                         model = "deepseek-v4-flash",
                         messages = chat_history,                # messages 是 API 关键字
                         temperature = 0.8
@@ -68,18 +90,22 @@ def on_message(ws, message):         # won_message：是库定义的关键字参
                 # 图片信息处理
 
                 elif segment_type == "image":
-                    message_image_url = segment.get("data", {}).get("url")
-                    chat_history.append({"role": "user", "content": [{"type": "image_url", 
-                                                                      "image_url":{"url": message_image_url} 
-                                                                      }] })     # "user"、"type"、"image_url"、"url"是关键字，后两个分别是 对象、键名
-                                                                                # OpenAI里规定 type 是 image_url 而不是 image
-                    ai_response = client.chat.completions.create (
-                        model = "qwen3-vl-plus",
-                        messages = chat_history,
-                        temperature = 0.8
-                    )
-                    ai_reply = ai_response.choices[0].message.content
-
+                    message_image_url = segment.get("data", {}).get("url")          # 获取图片 url
+                    print("正在提取图片并转码...")
+                    message_image_base64 = url_to_base64(message_image_url)         # 下载图片的 base64
+                    if message_image_base64:
+                        chat_history.append({"role": "user", "content": [{"type": "image_url", 
+                                                                          "image_url":{"url": message_image_base64} 
+                                                                          }] })     # "user"、"type"、"image_url"、"url"是关键字，后两个分别是 对象、键名
+                                                                                    # OpenAI里规定 type 是 image_url 而不是 image
+                        ai_response = client_qw.chat.completions.create (
+                            model = "qwen3-vl-plus",
+                            messages = chat_history,
+                            temperature = 0.8
+                        )
+                        ai_reply = ai_response.choices[0].message.content
+                    else:
+                        ai_reply = "图片好像加载失败了笨蛋...> <"         # 新增：加一个兜底回复，防止程序崩溃卡死
 
 
                 print(f"小夕回复: {ai_reply}")       # 日志信息，不会发到qq
@@ -121,3 +147,4 @@ if __name__ == "__main__":        # 这是 Python 的固定写法：只有直接
     ws.run_forever()            # 启动循环，持续监听来自 QQ 的消息。程序会一直运行，直到你关闭它。
 
     # 整体作用： 把你的 Python 程序连接到本地的 NapCatQQ 客户端，然后一直等待 QQ 消息并处理。
+
